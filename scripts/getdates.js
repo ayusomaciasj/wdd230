113cd63614d317537bcaf0367947262e6469c017
ayusomaciasj <ayuso.jd@gmail.com>
2024-05-23 15:34:38 -0600

    fixed script in bom

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,18 +1,16 @@
 //function to populate the current year
-function populateCurrentYear(){
+function populateCurrentYear() { 
     var currentYear = new Date().getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
-
 }
 
 //function to populate the last modified date
-function populateLastModified(){
+function populateLastModified() {
     var lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 }
 
 //Call the functions to populate the current year and last modified date
-
 populateCurrentYear();
 populateLastModified();
 
